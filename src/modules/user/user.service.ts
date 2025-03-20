a738d35337d8acabe4d17677770a20b382d6ff10
Otavio Bueno <joseotaviocbueno@gmail.com>
2025-03-20 09:31:37 -0300

    initial commit

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  private userDb = [
+    {
+      id: 1,
+      name: 'Otavio Bueno',
+      email: 'otavio.bueno@gmail.com',
+      password: '123456',
+    },
+    {
+      id: 2,
+      name: 'Otávio Bueno',
+      email: 'otavio.bueno@gmail.com',
+      password: '123456',
+    },
+    {
+      id: 3,
+      name: 'Otávio Bueno',
+      email: 'otavio.bueno@gmail.com',
+      password: '123456',
+    },
+    {
+      id: 4,
+      name: 'João Bueno',
+      email: 'joao.bueno@gmail.com',
+      password: '123456',
+    },
+    {
+      id: 5,
+      name: 'João Bueno',
+      email: 'joao.bueno@gmail.com',
+      password: '123456',
+    },
+  ];
+
+  async create(createUserDto: CreateUserDto) {}
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
